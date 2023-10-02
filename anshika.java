83a20517765ca00ae0ba312a1ce979b5be5afb1e
ANSHI14a <anshikasrivastava14903@gmail.com>
2023-10-02 22:30:01 +0530

    Add i/o file

--- anshika.java
+++ anshika.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
 public class anshika{
-    public static void main(String[] args) {
-        System.out.println("Hello");
-    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the array");
+        int n=sc.nextInt();
+        System.out.println("Enter the array elements");
+        int arr[]=new int[n];
 
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+
+        }
+        System.out.println("Elements are");
+
+        for(int i=0;i<n;i++)
+        System.out.println(arr[i]);
+    }
 }
 
